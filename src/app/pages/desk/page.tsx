c184ba2ebb087ee718517bddab51d10bdef99965
Sid Banerjee <sidbanerjee@berkeley.edu>
2024-09-16 23:41:46 -0700

    SSR Update

--- src/app/pages/desk/page.tsx
+++ src/app/pages/desk/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 import dynamic from 'next/dynamic'
-
-import LetterEditor from "@/app/components/LetterEditor";
  
 const DynamicComponentWithNoSSR = dynamic(
   () => import('@/app/components/LetterEditor'),
